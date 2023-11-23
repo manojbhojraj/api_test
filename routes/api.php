438e6a18437064e93dfebe4765ad73b10a5c2c7c
Manoj Kumar <khatrimanoj5171@gmail.com>
2023-11-23 19:34:57 +0500

    api test

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+Route::resource('products', ProductController::class);
+
+Route::middleware('auth:sanctum')->group( function () {
+});
